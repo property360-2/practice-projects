a926e633fc627334fec588e091d9cb0233bcf896
Jun Alvior <junalvior21@gmail.com>
2025-05-20 22:18:18 -1200

    book mark manager project initiated

---
+++ bookMark-manager/script.js
@@ -0,0 +1,21 @@
+const dropdown = document.getElementById("category-dropdown");
+const ViewButton = document.getElementById("view-category-button");
+const AddButton = document.getElementById("add-bookmark-button");
+
+
+const bookmarkCategory = new Map();
+const bookmark = new Set();
+
+console.log(bookmark);
+
+dropdown.addEventListener("change", () => 
+    console.log(dropdown.value)
+);
+
+const ViewCategory = () => {
+
+};
+
+const addBookmark = () => {
+
+};
